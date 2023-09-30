8887aef62081b90b73b391643e0e5f76b9077346
boogoogle <1099594257@qq.com>
2023-09-30 15:46:57 +0800

    chore: vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-let PORT = 3339;
+let PORT = 3000;
 if (process.env.MODE === 'PROD') {
-  PORT = 3340;
+  PORT = 3000;
 }
 
 async function bootstrap() {
